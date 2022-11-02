bb6fa4cec53449d3c39b96b293c7e5b9ec8541c2
htbscHKBen <116170916+htbscHKBen@users.noreply.github.com>
2022-11-02 17:42:33 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,10 @@
+IR.onReceivedIR(function () {
+    basic.showString(IR.getRecType())
+    basic.showString(IR.getMessage())
+})
+IR.setREC_pin(
+DigitalPin.P8
+)
+basic.forever(function () {
+	
+})
